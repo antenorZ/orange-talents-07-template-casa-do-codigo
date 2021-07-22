910d1ae9d2ac1660a0715e72c3f7be89f45f06bf
Marcos Antenor <marcos.souza@zup.com.br>
2021-07-22 09:42:58 -0300

    fix: findByEmail retornando Optional<Autor>

--- casadocodigo/src/main/java/br/com/zup/casadocodigo/repository/AutorRepository.java
+++ casadocodigo/src/main/java/br/com/zup/casadocodigo/repository/AutorRepository.java
@@ -1,12 +1,14 @@
 package br.com.zup.casadocodigo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.zup.casadocodigo.model.Autor;
 
 public interface AutorRepository extends JpaRepository<Autor, Long>{
 
-	Autor findByemail(String email);
+	Optional<Autor> findByemail(String email);
 
 	boolean existsByemail(String email);
 	
